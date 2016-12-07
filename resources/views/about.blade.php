7e687c5eb6628b38ec6a956912890220bf42589b
Carlos Devletian <carlosdevletian@gmail.com>
2016-12-07 17:47:44 -0400

    Inicio de styling de about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,9 +5,13 @@
 @endsection
 
 @section('content')
-    <div class="container" style="position: relative; margin-top: 10px">
-        <h1 class="text-center">About Matex</h1>
-        <h2 style="background-color: rgba(255,255,255,0.7); border-radius: 5px; padding: 20px; text-align: center; font-size: 24px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus optio voluptatum iure numquam explicabo architecto quibusdam et nihil vel rerum praesentium mollitia, libero quod quo in natus, magni nesciunt itaque!</h2>
+<div style="margin-top: -50px;"></div>
+<div class="About">
+    <div class="container" style="position: relative;">
+        <h1 class="About__title">About Us</h1>
+        <h2 class="About__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus optio voluptatum iure numquam explicabo architecto quibusdam et nihil vel rerum praesentium mollitia, libero quod quo in natus, magni nesciunt itaque!</h2>
+        <h2 class="About__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus optio voluptatum iure numquam explicabo architecto quibusdam et nihil vel rerum praesentium mollitia, libero quod quo in natus, magni nesciunt itaque!</h2>
+    </div>
         <!-- <div style="position: absolute; right:0; text-align: right" >
             <h2 style="color: red; margin-bottom: -10px">LOREM IPSUM</h2>
             <h2 style="color: #0000AA; margin-top: -5px; margin-bottom: -10px">DOLOR SIT AMET</h2>
@@ -18,7 +22,8 @@
             <h2 style="color: #F16A01; margin-top: -5px; margin-bottom: -10px">CONSECTEUR</h2>
             <h2 style="color: black; margin-top: -5px; margin-bottom: -10px">ADIPISICING</h2>
         </div> -->
-    </div>
-    <img src="/images/modal/bracelets.png" style="position: fixed; top: 0; z-index: -1; height: 100%" >
+</div>
+<div class="About__image"></div>
+    <!-- <img src="/images/modal/bracelets.png" style="position: fixed; top: 0; height: 100%" > -->
 
 @endsection
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,9 @@
-<nav class="navbar navbar-default navbar-fixed-top {{ Route::currentRouteName() == 'home' ? 'navbar-home' : '' }}">
+<nav class="navbar navbar-default navbar-fixed-top {{ Route::currentRouteName() == 'home' ? 'navbar-home' : '' }} {{ Route::currentRouteName() == 'about' ? 'navbar-about' : ''}}">
 	<div class="container">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
 				<span class="sr-only">Toggle navigation</span>
-                @if(Route::currentRouteName() == 'home') 
+                @if(Route::currentRouteName() == 'home' || Route::currentRouteName() == 'about') 
                     <span class="icon-bar" style="background-color: white"></span>
                     <span class="icon-bar" style="background-color: white"></span>
                     <span class="icon-bar" style="background-color: white"></span>
@@ -18,11 +18,11 @@
 			</a>
 		</div>
 		<div id="navbar" class="collapse navbar-collapse navbar-home">
-			<ul class="nav navbar-nav {{ Route::currentRouteName() == 'home' ? 'navbar-home' : '' }}">
+			<ul class="nav navbar-nav {{ Route::currentRouteName() == 'home' ? 'navbar-home' : '' }} {{ Route::currentRouteName() == 'about' ? 'navbar-about' : ''}}">
 				<li><a href="{{ route('about') }}">About</a></li>
 				<li><a @click=" { openContactModal() } " role="button">Contact Us</a></li>
 			</ul>
-            <ul class="nav navbar-nav navbar-right {{ Route::currentRouteName() == 'home' ? 'navbar-home' : '' }}">
+            <ul class="nav navbar-nav navbar-right {{ Route::currentRouteName() == 'home' ? 'navbar-home' : '' }} {{ Route::currentRouteName() == 'about' ? 'navbar-about' : '' }}">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>

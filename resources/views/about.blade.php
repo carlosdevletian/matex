726a89b105fed86c70ec9ac3e7115844fdc39df0
Carlos Devletian <carlosdevletian@gmail.com>
2016-12-09 10:47:38 -0400

    timeline

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,24 +6,97 @@
 
 @section('content')
 <div style="margin-top: -50px;"></div>
-<div class="About">
-    <div class="container" style="position: relative;">
-        <h1 class="About__title">About Us</h1>
-        <h2 class="About__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus optio voluptatum iure numquam explicabo architecto quibusdam et nihil vel rerum praesentium mollitia, libero quod quo in natus, magni nesciunt itaque!</h2>
-        <h2 class="About__body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus optio voluptatum iure numquam explicabo architecto quibusdam et nihil vel rerum praesentium mollitia, libero quod quo in natus, magni nesciunt itaque!</h2>
-    </div>
-        <!-- <div style="position: absolute; right:0; text-align: right" >
-            <h2 style="color: red; margin-bottom: -10px">LOREM IPSUM</h2>
-            <h2 style="color: #0000AA; margin-top: -5px; margin-bottom: -10px">DOLOR SIT AMET</h2>
-            <h2 style="color: #F16A01; margin-top: -5px; margin-bottom: -10px">CONSECTEUR</h2>
-            <h2 style="color: black; margin-top: -5px; margin-bottom: -10px">ADIPISICING</h2>
-            <h2 style="color: red; margin-top: -5px; margin-bottom: -10px">LOREM IPSUM</h2>
-            <h2 style="color: #0000AA; margin-top: -5px; margin-bottom: -10px">DOLOR SIT AMET</h2>
-            <h2 style="color: #F16A01; margin-top: -5px; margin-bottom: -10px">CONSECTEUR</h2>
-            <h2 style="color: black; margin-top: -5px; margin-bottom: -10px">ADIPISICING</h2>
-        </div> -->
-</div>
-<div class="About__image"></div>
-    <!-- <img src="/images/modal/bracelets.png" style="position: fixed; top: 0; height: 100%" > -->
 
+<div class="About">
+    <div class="container">
+        <div class="row">
+            <h1 class="col-sm-10 About__title">we strive to make quality products</h1>
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2 About__quote neg-marg-top">our process...</div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-xs-5 About__time">
+            <div class="row About__time__box">
+                <img src="/images/about/about_design.png" class="img-responsive About__time__image">
+            </div>
+            <div class="row About__time__box"> 
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
+            </div>
+            <div class="row About__time__box">
+                <img src="/images/about/about_factory.png" class="img-responsive About__time__image">
+            </div>
+            <div class="row About__time__box">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
+            </div>
+        </div>
+        <div class="col-xs-2">
+            <img src="/images/about/about_timeline.png" class="img-responsive About__time__line">
+        </div>
+        <div class="col-xs-5 About__time">
+            <div class="row About__time__box">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
+            </div>
+            <div class="row About__time__box">
+                <img src="/images/about/about_assist.png" class="img-responsive About__time__image">
+            </div>
+            <div class="row About__time__box">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
+            </div>
+            <div class="row About__time__box">
+                <img src="/images/about/about_shipping.png" class="img-responsive About__time__image">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- 
+<div class="container">
+    <div class="row">
+        <div class="col-xs-5 text-center About__time">
+            <img src="/images/about/about_design.png" class="img-responsive About__time__image">
+            <div style="margin-top: 150px; font-size: 1.5em; margin-bottom: 125px;">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
+            </div>
+            <img src="/images/about/about_factory.png" class="img-responsive About__time__image">
+            <div style="margin-top: 150px; font-size: 1.5em;">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
+            </div>
+        </div>
+        <div class="col-xs-2">
+            <img src="/images/about/about_timeline.png" class="img-responsive" style="margin: 0 auto; max-height: 1100px">
+        </div>
+        <div class="col-xs-5 text-center" style="padding: 60px">
+            <div style="margin-bottom: 150px; font-size: 1.5em;">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
+            </div>
+            <img src="/images/about/about_assist.png" class="img-responsive About__time__image">
+            <div style="margin-bottom: 150px; font-size: 1.5em; margin-top: 125px">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
+            </div>
+            <img src="/images/about/about_shipping.png" class="img-responsive About__time__image">
+        </div>
+    </div>
+</div> -->
+<div class="container">
+    <div class="row">
+        <div class="col-xs-8 col-xs-offset-2 About__quote" style="border-color: #0000AA; color: #0000AA;">our background...</div>
+    </div>
+</div>
+<div class="Landing neg-marg-top">
+    <div class="container" style="">
+        <div class="row">
+            <h1 class="col-sm-12 About__title">
+                Lorem ipsum dolor sit amet, consectetur adipisicing
+            </h1><!-- 
+            <img src="/images/about/about_world.png" class="img-responsive" style="max-width: 400px; margin: 0 auto; background-color: white; border-radius: 100%"> -->
+        </div>
+    </div>
+</div>
 @endsection

346eeeedbdc60718a06214b08ea58965d136a7ef
akoury <Alejandro@Alejandros-MacBook.local>
2016-12-12 12:34:21 -0400

    Arreglado Timeline en About (Hacer gulp)

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -21,69 +21,31 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-xs-5 About__time">
-            <div class="row About__time__box">
-                <img src="/images/about/about_design.png" class="img-responsive About__time__image">
-            </div>
-            <div class="row About__time__box"> 
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
-            </div>
-            <div class="row About__time__box">
-                <img src="/images/about/about_factory.png" class="img-responsive About__time__image">
-            </div>
-            <div class="row About__time__box">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <img src="/images/about/about_timeline.png" class="img-responsive About__time__line">
-        </div>
-        <div class="col-xs-5 About__time">
-            <div class="row About__time__box">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
-            </div>
-            <div class="row About__time__box">
-                <img src="/images/about/about_assist.png" class="img-responsive About__time__image">
-            </div>
-            <div class="row About__time__box">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
-            </div>
-            <div class="row About__time__box">
-                <img src="/images/about/about_shipping.png" class="img-responsive About__time__image">
-            </div>
-        </div>
-    </div>
+<div class="container-fluid">
+    <table class="About__time">
+        <tr>
+            <td class="About__time__cell"><img src="/images/about/about_design.png" class="img-responsive About__time__image"></td>
+            <td class="About__time__cell--line"><img src="/images/about/about_circle.png" class="img-responsive About__time__image--circle"></td>
+            <td class="About__time__cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat</td>
+        </tr>
+        <tr>
+            <td class="About__time__cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat</td>
+            <td class="About__time__cell--line"><img src="/images/about/about_circle.png" class="img-responsive About__time__image--circle"></td>
+            <td class="About__time__cell"><img src="/images/about/about_assist.png" class="img-responsive About__time__image"></td>
+        </tr>
+        <tr>
+            <td class="About__time__cell"><img src="/images/about/about_factory.png" class="img-responsive About__time__image"></td>
+            <td class="About__time__cell--line"><img src="/images/about/about_circle.png" class="img-responsive About__time__image--circle"></td>
+            <td class="About__time__cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat</td>
+        </tr>
+        <tr>
+            <td class="About__time__cell">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat</td>
+            <td class="About__time__cell--line"><img src="/images/about/about_circle.png" class="img-responsive About__time__image--circle"></td>
+            <td class="About__time__cell"><img src="/images/about/about_shipping.png" class="img-responsive About__time__image"></td>
+        </tr>
+    </table>
 </div>
-<!-- 
-<div class="container">
-    <div class="row">
-        <div class="col-xs-5 text-center About__time">
-            <img src="/images/about/about_design.png" class="img-responsive About__time__image">
-            <div style="margin-top: 150px; font-size: 1.5em; margin-bottom: 125px;">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
-            </div>
-            <img src="/images/about/about_factory.png" class="img-responsive About__time__image">
-            <div style="margin-top: 150px; font-size: 1.5em;">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus fugiat
-            </div>
-        </div>
-        <div class="col-xs-2">
-            <img src="/images/about/about_timeline.png" class="img-responsive" style="margin: 0 auto; max-height: 1100px">
-        </div>
-        <div class="col-xs-5 text-center" style="padding: 60px">
-            <div style="margin-bottom: 150px; font-size: 1.5em;">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
-            </div>
-            <img src="/images/about/about_assist.png" class="img-responsive About__time__image">
-            <div style="margin-bottom: 150px; font-size: 1.5em; margin-top: 125px">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus accusantium ex repellendus
-            </div>
-            <img src="/images/about/about_shipping.png" class="img-responsive About__time__image">
-        </div>
-    </div>
-</div> -->
+
 <div class="container">
     <div class="row">
         <div class="col-xs-8 col-xs-offset-2 About__quote" style="border-color: #0000AA; color: #0000AA;">our background...</div>
@@ -94,7 +56,7 @@
         <div class="row">
             <h1 class="col-sm-12 About__title">
                 Lorem ipsum dolor sit amet, consectetur adipisicing
-            </h1><!-- 
+            </h1><!--
             <img src="/images/about/about_world.png" class="img-responsive" style="max-width: 400px; margin: 0 auto; background-color: white; border-radius: 100%"> -->
         </div>
     </div>

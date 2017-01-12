637adfb5d615ef9b9b67a75c4ca1cd27171e9bf4
akoury <Alejandro@Alejandros-MacBook.local>
2017-01-12 00:33:55 -0400

    Agregado FPD con errores. Hacer Yarn y Gulp
    La opción utilizada para designar la carpeta de html's para FPD se
    llama templatesDirectory
    (http://jquerydoc.fancyproductdesigner.com/classes/FancyProductDesignerO
    ptions.defaults.html#property_templatesDirectory) pero no esta
    funcionando.

--- resources/views/designs/create.blade.php
+++ resources/views/designs/create.blade.php
@@ -4,13 +4,47 @@
     Design your {{ $category->name }}
 @endsection
 
+@push('styles')
+    <!-- Main CSS for the product designer -->
+    <link rel="stylesheet" type="text/css" href="{{ URL::to("css/FancyProductDesigner-all.min.css") }}" />
+@endpush
+
 @section('content')
+
     <div class="container">
         <div class="row">
             <h2>Design your {{ $category->name }}</h2>
-            <ul>
-                {{-- FPD --}}
-            </ul>
+
+            <div id="fpd">
+
+            </div>
+
         </div>
     </div>
 @endsection
+
+@push('scripts')
+
+    {{-- Jquery UI --}}
+    <script src="http://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
+    {{-- Fabric.js --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fabric.js/1.7.2/fabric.min.js" integrity="sha256-ZoIcsJVfAnLPf/ZgnJtJWarvXjiy4qBpTME0V1QLWD8=" crossorigin="anonymous"></script>
+
+    {{-- FPD --}}
+    <script src="{{ URL::to("js/FancyProductDesigner-all.min.js") }}" type="text/javascript"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            var $fpd = $('#fpd'),
+                pluginOpts = {
+                    stageWidth: 1200,
+                    stageHeight: 800,
+                    langJSON: "{{ URL::to('default.json') }}",
+                    templatesDirectory: "{{ URL::to('fpd') . '/'}}",
+                };
+
+            var yourDesigner = new FancyProductDesigner($fpd, pluginOpts);
+        });
+    </script>
+@endpush

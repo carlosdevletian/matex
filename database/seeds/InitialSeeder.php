46ca381b96e05a46a65cf04e14f5024f824ada23
Carlos Devletian <carlosdevletian@gmail.com>
2017-01-18 10:48:45 -0400

    Modificado Seeder Inicial (Category, Design y Product)

--- database/seeds/InitialSeeder.php
+++ database/seeds/InitialSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\User;
-use App\Product;
+use App\Models\User;
+use App\Models\Design;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class InitialSeeder extends Seeder
@@ -14,5 +16,13 @@ class InitialSeeder extends Seeder
     public function run()
     {
         $user = User::create(['name' => 'Alejandro', 'email' => 'alkv93@gmail.com', 'password' => bcrypt('123123')]);
+
+        $category = factory(Category::class)->create(['name' => 'Bracelets']);
+        
+        $design = factory(Design::class)->create(['image_name' => 'test_filename.jpg']);
+        
+        $product = factory(Product::class)->create(['name' => 'Small', 'category_id' => $category->id]);
+        $product2 = factory(Product::class)->create(['name' => 'Medium', 'category_id' => $category->id]);
+        $product3 = factory(Product::class)->create(['name' => 'Large', 'category_id' => $category->id]);
     }
 }
